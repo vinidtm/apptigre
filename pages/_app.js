5c4d30a4ba50e640bc54b8697494cf8cc0d72750
Vinidtm <vinidtm@hotmail.com>
2023-10-26 19:28:56 -0300

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -14,34 +14,16 @@ export default function MyApp({ Component, pageProps }) {
           {/* Google Tag Manager */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MF42VBT');`,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N6ZH3Q2Q');`,
             }}
           />
-          {/* End Google Tag Manager */}
-          {/* Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-KK3W3K20DY`}
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-KK3W3K20DY', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />    
+          {/* End Google Tag Manager */}  
     </Head>
     <Component {...pageProps}/>
         {/* Google Tag Manager (noscript) */}
         <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MF42VBT" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6ZH3Q2Q" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
           {/* End Google Tag Manager (noscript) */}
