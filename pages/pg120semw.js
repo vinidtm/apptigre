2c34e10bf36d50696cbecc931cc3a08d0bae9be9
Vinidtm <vinidtm@hotmail.com>
2023-11-13 19:36:33 -0300

    semwhats

---
+++ pages/pg120semw.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Head from 'next/head';
+import Video120semw from './video120semw';
+
+
+const Pg120semw = () => {
+  return (
+    <>
+    <Head>
+    <title>Tiger S </title>
+    <link rel='shortcut icon' href='/Icon.jpg' />
+
+<link rel="preload" href="https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/653c0b4909054e00092a6b14/player.js" as="script"/>
+<link rel="preload" href="https://cdn.converteai.net/lib/js/smartplayer/v1/smartplayer.min.js" as="script"/>
+<link rel="preload" href="https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/653c0b4909054e00092a6b14/thumbnail.jpg" as="image"/>
+<link rel="preload" href="https://cdn.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/653c0b3e67a28d0008192ed0/main.m3u8" as="fetch"/>
+<link rel="dns-prefetch" href="https://cdn.converteai.net"/>
+<link rel="dns-prefetch" href="https://scripts.converteai.net"/>
+<link rel="dns-prefetch" href="https://images.converteai.net"/>
+<link rel="dns-prefetch" href="https://api.vturb.com.br"/>
+
+    </Head>
+    <div>
+    <div className='video-player-container-tigre'>
+    <Video120semw/>
+    </div>
+    </div>
+    </>
+  );
+};
+
+export default Pg120semw;
+
+
---
+++ pages/video120semw.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react';
+import Utm7k from '../public/utm7k';
+
+function Video120semw() {
+  const [areSectionsDisplayed, setAreSectionsDisplayed] = useState(false);
+
+  useEffect(() => {
+    const SECONDS_TO_DISPLAY = 23;
+    const CLASS_TO_DISPLAY = '.esconder';
+
+    let attempts = 0;
+    let elsHiddenList = [];
+    let elsDisplayed = false;
+    const elsHidden = document.querySelectorAll(CLASS_TO_DISPLAY);
+    const alreadyDisplayedKey = `alreadyElsDisplayed${SECONDS_TO_DISPLAY}`;
+    const alreadyElsDisplayed = localStorage.getItem(alreadyDisplayedKey);
+
+    setTimeout(() => {
+      elsHiddenList = Array.from(elsHidden);
+    }, 0);
+
+    const showHiddenElements = () => {
+      elsDisplayed = true;
+      elsHiddenList.forEach((e) => (e.style.display = 'block'));
+      localStorage.setItem(alreadyDisplayedKey, 'true');
+      setAreSectionsDisplayed(true);
+    };
+
+    const startWatchVideoProgress = () => {
+      if (
+        typeof smartplayer === 'undefined' ||
+        !(smartplayer.instances && smartplayer.instances.length)
+      ) {
+        if (attempts >= 10) return;
+        attempts += 1;
+        return setTimeout(startWatchVideoProgress, 1000);
+      }
+
+      smartplayer.instances[0].on('timeupdate', () => {
+        if (elsDisplayed || smartplayer.instances[0].smartAutoPlay) return;
+        if (smartplayer.instances[0].video.currentTime < SECONDS_TO_DISPLAY)
+          return;
+        showHiddenElements();
+      });
+    };
+
+    if (alreadyElsDisplayed === 'true') {
+      setTimeout(() => {
+        showHiddenElements();
+      }, 100);
+    } else {
+      startWatchVideoProgress();
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, []);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.id = 'scr_653c0b4909054e00092a6b14';
+    script.src =
+      'https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/653c0b4909054e00092a6b14/player.js';
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div
+        id='vid_653c0b4909054e00092a6b14'
+        style={{ position: 'relative', width: '100%', padding: '178.21782178217822% 0 0' }}
+      >
+        <img
+          id='thumb_653c0b4909054e00092a6b14'
+          src='https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/653c0b4909054e00092a6b14/thumbnail.jpg'
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            display: 'block',
+          }}
+        />
+        <div
+          id='backdrop_653c0b4909054e00092a6b14'
+          style={{
+            position: 'absolute',
+            top: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        ></div>
+      </div>
+      {areSectionsDisplayed && (
+          <div className='button-cont'>
+          <a href="https://app.chatonline.chat/tiger-30-120" className="custom-button">
+            RECEBA SEU PRÊMIO AGORA!</a>
+            <Utm7k/>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Video120semw;
